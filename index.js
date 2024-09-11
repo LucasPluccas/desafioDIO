85142cad2e960a7b981d5caea71afd6844d7f448
LucasPluccas <lucaspereira330@gmail.com>
2024-09-10 22:59:04 -0300

    enunciado do desafio

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Olá, mundo e Brasil")
+// Crie uma variável para armazenar o nome e a quantidade de XP do herói, depois utilize uma estrutura de decisão para apresentar alguma mensagem abaixo
+
+//Se XP for < que 1.000 = Ferro
+//XP entre 1.001 e 2.000 = Bronze
+//XP entre 2.001 e 5.000 = Prata
+//XP entre 6.001 e 7.000= Ouro
+//XP entre 7.001 e 8.000 = Platina
+//XP entre 8.001 e 9.000 = Ascendente
+//XP entre 9.001 e 10.000 = Imortal
+//XP > ou igual a 10.001 = Radiante
+
+//#saída
+//printar uma mensagem:
+// O herói de nome (nome) está no nível de (nivel)
