ad96ee4b5b111ee8c3cb4365cf9df4604eb914f5
Lucas Pereira <165937362+LucasPluccas@users.noreply.github.com>
2024-09-13 13:05:49 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //Se XP for < que 1.000 = Ferro
 //XP entre 1.001 e 2.000 = Bronze
 //XP entre 2.001 e 5.000 = Prata
-//XP entre 6.001 e 7.000= Ouro
+//XP entre 5.001 e 7.000= Ouro
 //XP entre 7.001 e 8.000 = Platina
 //XP entre 8.001 e 9.000 = Ascendente
 //XP entre 9.001 e 10.000 = Imortal
@@ -14,18 +14,30 @@
 // O herói de nome (nome) está no nível de (nivel)
 
 let name ="Lucas Kenobi"
-let XP = "800"
+let xp = ""
 let nivel =""
 
-switch(XP){
-    case(XP <1.000):
+switch(xp){
+    case (XP < 1.000):
         nivel = "Ferro"
         break;
-case(XP >1.000 <2.000):
-nível = "Bronze"
+    case (xp >1.000 && xp <2.001):
+    nivel = "Bronze"
 break;
-
+    case (xp > 2.000 && xp < 5.001):
+     nivel = "Prata"
+    case (xp > 5.000 && xp < 7.000):
+     nivel = "Ouro"
+    case (xp > 7.000 && xp < 8.001):
+     nivel ="Platina"
+    case (xp > 8.000 && xp < 9.001):
+     nivel = "Ascendente"
+    case (xp > 9.000 && xp < 10.001):
+     nivel="Imortal"
+    case (xp >= 10.001):
+     nivel="Radiante"
         default:
+console.log("Insira o xp do herói)
 }
 
 
