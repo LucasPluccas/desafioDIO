33cafa88b0b6de305631e3fe5888682a5fef1dc9
Lucas Pereira <165937362+LucasPluccas@users.noreply.github.com>
2024-09-13 13:22:27 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ switch (true) {
     case (xp < 1000):
         nivel = "Ferro"
         break;
-    case (xp >1000 && xp <2001):
+    case (xp >=1000 && xp <=2000):
     nivel = "Bronze"
         break;
-    case (xp > 2000 && xp < 5001):
+    case (xp > 2000 && xp <= 5000):
      nivel = "Prata"
         break;
-    case (xp > 5000 && xp < 7001):
+    case (xp > 5000 && xp <= 7000):
      nivel = "Ouro"
         break;
-    case (xp > 7000 && xp < 8001):
+    case (xp > 7000 && xp <= 8000):
      nivel ="Platina"
         break;
-    case (xp > 8000 && xp < 9001):
+    case (xp > 8000 && xp <= 9000):
      nivel = "Ascendente"
         break;
-    case (xp > 9000 && xp < 10001):
+    case (xp > 9000 && xp <= 10000):
      nivel="Imortal"
         break;
     case (xp >= 10001):
