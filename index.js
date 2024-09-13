8e5db7a0e65adf9ca9cd4658269c3948dba60a8c
Lucas Pereira <165937362+LucasPluccas@users.noreply.github.com>
2024-09-13 13:19:35 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -9,19 +9,25 @@ switch (true) {
         break;
     case (xp >1000 && xp <2001):
     nivel = "Bronze"
-break;
+        break;
     case (xp > 2000 && xp < 5001):
      nivel = "Prata"
+        break;
     case (xp > 5000 && xp < 7001):
      nivel = "Ouro"
+        break;
     case (xp > 7000 && xp < 8001):
      nivel ="Platina"
+        break;
     case (xp > 8000 && xp < 9001):
      nivel = "Ascendente"
+        break;
     case (xp > 9000 && xp < 10001):
      nivel="Imortal"
+        break;
     case (xp >= 10001):
      nivel="Radiante"
+        break;
         default:
 console.log("Insira o xp do herói")
 }
