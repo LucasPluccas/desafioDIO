40a6c256eb1aed91d1c8e32701d1ffca03aba4a8
Lucas Pereira <165937362+LucasPluccas@users.noreply.github.com>
2024-09-13 13:14:50 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
-// Crie uma variável para armazenar o nome e a quantidade de XP do herói, depois utilize uma estrutura de decisão para apresentar alguma mensagem abaixo
-
-//Se XP for < que 1.000 = Ferro
-//XP entre 1.001 e 2.000 = Bronze
-//XP entre 2.001 e 5.000 = Prata
-//XP entre 5.001 e 7.000= Ouro
-//XP entre 7.001 e 8.000 = Platina
-//XP entre 8.001 e 9.000 = Ascendente
-//XP entre 9.001 e 10.000 = Imortal
-//XP > ou igual a 10.001 = Radiante
-
-//#saída
-//printar uma mensagem:
-// O herói de nome (nome) está no nível de (nivel)
 
 let name ="Lucas Kenobi"
 let xp = 
-let nivel =""
+let nivel = "";
 
-switch(xp){
-    case (XP < 1.000):
+switch (true) {
+    case (XP < 1000):
         nivel = "Ferro"
         break;
-    case (xp >1.000 && xp <2.001):
+    case (xp >1000 && xp <2001):
     nivel = "Bronze"
 break;
-    case (xp > 2.000 && xp < 5.001):
+    case (xp > 2000 && xp < 5001):
      nivel = "Prata"
-    case (xp > 5.000 && xp < 7.000):
+    case (xp > 5000 && xp < 7000):
      nivel = "Ouro"
-    case (xp > 7.000 && xp < 8.001):
+    case (xp > 7000 && xp < 8001):
      nivel ="Platina"
-    case (xp > 8.000 && xp < 9.001):
+    case (xp > 8000 && xp < 9001):
      nivel = "Ascendente"
-    case (xp > 9.000 && xp < 10.001):
+    case (xp > 9000 && xp < 10001):
      nivel="Imortal"
-    case (xp >= 10.001):
+    case (xp >= 10001):
      nivel="Radiante"
         default:
 console.log("Insira o xp do herói)
